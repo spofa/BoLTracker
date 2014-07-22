fa7a8ec8aa4c366d08f6f45fb7cc0d98d53a565f
dibes <dibesjr@gmail.com>
2014-07-22 15:11:41 -0400

    Attempt to speed up unique runs

--- app/controllers/RestController.php
+++ app/controllers/RestController.php
@@ -171,7 +171,7 @@ class RestController extends BaseController {
 
 	public function getUniqueruns($scriptName) {
 		$scriptDates = Script::where('created_at','>',Carbon::today()->subWeek())->where('script_name', '=', $scriptName)->where("owner_id", "=", Sentry::getUser()->id)->groupBy("hwid")->groupBy(DB::raw('DAY(created_at)'))->get(array('script_name', 'created_at'));
-		$scripts = Script::where('script_name', '=', $scriptName)->groupBy("hwid")->where("owner_id", "=", Sentry::getUser()->id)->get(array('script_name', 'created_at'));
+		$scripts = Script::where('created_at','>',Carbon::today()->subWeek())->where('script_name', '=', $scriptName)->groupBy("hwid")->where("owner_id", "=", Sentry::getUser()->id)->get(array('script_name', 'created_at'));
 		$datesArray = array();
 
 		foreach($scriptDates as $dates) {
